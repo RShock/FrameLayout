613da552d193c1e21969b8be8726b158f064bb23
fujian.xu <fujian.xu@thoughtworks.com>
2020-07-20 09:53:45 +0800

    初步成型

--- app/src/main/java/com/example/framelayout/MainActivity.java
+++ app/src/main/java/com/example/framelayout/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.framelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,20 +10,34 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button button1;
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button) findViewById(R.id.button1);
+        button1 = (Button) findViewById(R.id.button2);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openNewActivity();
+                openConstraintActivity();
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
             }
         });
     }
-    public void openNewActivity(){
+
+    public void openConstraintActivity(){
         Intent intent = new Intent(this, ConstraintActivity.class);
+        startActivity(intent);
+    }
+
+    public void openLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
